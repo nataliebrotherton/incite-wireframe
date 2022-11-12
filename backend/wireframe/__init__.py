c0e43daf713d3338f885ffef340c041505321c72
Natalie Brotherton <brona@umich.edu>
2022-11-12 17:28:56 -0500

    graph data api works

--- backend/wireframe/__init__.py
+++ backend/wireframe/__init__.py
@@ -1,10 +1,14 @@
 import os
+import pathlib
 
 from flask import Flask
 from flask_cors import CORS
 
 
 app = Flask(__name__)
+
+application_root = pathlib.Path(__file__).resolve().parent.parent
+app.config['DATA_DIR'] = application_root/'data'
 CORS(app)
 
 from wireframe.index import index
--- backend/wireframe/index.py
+++ backend/wireframe/index.py
@@ -4,9 +4,23 @@ Main view
 
 import flask
 import wireframe
+import pathlib
+import json
 
-@wireframe.app.route('/')
-def index():
-    """Display main page"""
-    return 'Hello'
+@wireframe.app.route('/data/<name>')
+def get_data(name):
+    """Retrieves dataset indicated by `name`. 
 
+    Args:
+        name (string): Indicates the name of the dataset to serve.
+    """
+
+    return get_json_file(name)
+
+def get_json_file(filename):
+    filename += '.json'
+    filename = pathlib.Path(wireframe.app.config['DATA_DIR'])/filename
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        print(data)
+    return data
