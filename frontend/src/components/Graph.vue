729b4b2af7f9094d9b4ff350433fec352cfcfc53
Natalie Brotherton <brona@umich.edu>
2022-11-15 12:21:46 -0500

    add default styles for chart, clean up base.css

--- frontend/src/components/Graph.vue
+++ frontend/src/components/Graph.vue
@@ -1,10 +1,34 @@
 <script lang="ts">
-// Controls rendering graphs
+// Controls rendering graphs, global styling
 import GraphData from './GraphData';
 
 import BarChart from './Charts/BarChart.vue';
 import LineChart from './Charts/LineChart.vue';
 import PieChart from './Charts/PieChart.vue';
+
+// set default global chart styles
+import { Chart } from 'chart.js';
+
+const setChartDefaults = function() {
+    let style = getComputedStyle(document.body);
+
+    let text_label_color = style.getPropertyValue('--text-label-1');
+    Chart.defaults.color = text_label_color;
+
+    let font_weight = style.getPropertyValue('--font-weight');
+    Chart.defaults.font.weight = font_weight;
+
+    Chart.defaults.font.family = "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif";
+
+    // hide gridlines
+    Chart.defaults.scale.grid.display = false;
+
+    // hide legend
+    Chart.defaults.plugins.legend.display = false;
+    console.log(Chart.defaults);
+};
+
+setChartDefaults();
 
 export default {
     props: {
@@ -29,7 +53,6 @@ export default {
 <template>
     <div :id="graphName" class="graph">
         <p class="label">{{ graphData.getLabel() }}</p>
-        {{graphName}}
         <BarChart v-if="graphData.isBar()" :graph-data="graphData" />
         <LineChart v-else-if="graphData.isLine()" :graph-data="graphData" />
         <PieChart v-else-if="graphData.isPie()" :graph-data="graphData" />
