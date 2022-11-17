9734c9172e4f86a074358c5bf27951755b4f57a7
Natalie Brotherton <brona@umich.edu>
2022-11-16 22:54:46 -0500

    add logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,8 +12,10 @@ export default {
 
 <template>
   <main>
+    <div id="logo"><img src="/src/assets/logo_full.png"  /></div>
     <p class="main title">Analysis of 2016-17 TSheets Data</p>
     <Overview />
     <GraphView />
+    <div id="logo-bottom"><img src="/src/assets/logo.png" /></div>
   </main>
 </template>
